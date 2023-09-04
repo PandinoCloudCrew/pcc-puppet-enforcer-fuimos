606366845a9e056a110697ff4f6b989f42bdae3c
Yesid Bocanegra <yesid.bocanegra@pandino.co>
2023-09-04 13:43:20 -0500

    :wrench: update to new server values

--- spring-run.sh
+++ spring-run.sh
@@ -7,7 +7,7 @@ export SPRING_CLOUD_VAULT_HOST=vault-dev.ops.pandino.co
 export SPRING_CLOUD_VAULT_KV_BACKEND=local
 export SPRING_CLOUD_VAULT_PORT=443
 export SPRING_CLOUD_VAULT_SCHEME=https
-export SPRING_CLOUD_VAULT_TOKEN=hvs.QJswihsgYxhRc08qKyLama3l
+export SPRING_CLOUD_VAULT_TOKEN=hvs.nQfakaPku2lwZmejkN6z1SPS
 export SPRING_CONFIG_IMPORT=vault://
 
 VERSION=$(./gradlew properties -q | awk '/^version:/ {print $2}')
